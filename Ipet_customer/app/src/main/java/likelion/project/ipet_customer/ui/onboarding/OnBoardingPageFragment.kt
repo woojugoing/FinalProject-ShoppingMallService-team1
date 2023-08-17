5292535ed9f8a2aa75446d07646920d215d402fd
dltkd1395 <dltkd13956@naver.com>
2023-08-17 15:45:12 +0900

    Feat : 온보딩 ViewPager 구현 #12

---
+++ Ipet_customer/app/src/main/java/likelion/project/ipet_customer/ui/onboarding/OnBoardingPageFragment.kt
@@ -0,0 +1,127 @@
+package likelion.project.ipet_customer.ui.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import likelion.project.ipet_customer.databinding.FragmentOnBoardingPageBinding
+
+const val ARG_PARM1 = "param1"
+const val ARG_PARM2 = "param2"
+const val ARG_PARM3 = "param3"
+
+class OnBoardingPageFragment : Fragment() {
+
+    lateinit var onBoardingPageBinding: FragmentOnBoardingPageBinding
+    private var descripts: Array<String>? = null
+    private var lottieResIds: IntArray? = null
+    private var page: Int? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        onBoardingPageBinding = FragmentOnBoardingPageBinding.inflate(inflater)
+        arguments?.let {
+            descripts = it.getStringArray(ARG_PARM1)
+            lottieResIds = it.getIntArray(ARG_PARM2)
+            page = it.getInt(ARG_PARM3)
+        }
+        setOnboarding()
+        return onBoardingPageBinding.root
+    }
+
+    private fun setOnboarding() {
+        onBoardingPageBinding.run {
+            when (page) {
+                OnboardingPage.FIRST_PAGE.position -> showFirstPage()
+                OnboardingPage.SECOND_PAGE.position -> showSecondPage()
+                OnboardingPage.THIRD_PAGE.position -> showThirdPage()
+                else -> showFourthPage()
+            }
+        }
+    }
+
+    private fun showFirstPage() {
+        onBoardingPageBinding.run {
+            lottieAnimationViewOnboardingpageFirst.apply {
+                lottieResIds?.let {
+                    setAnimation(it[0])
+                }
+                playAnimation()
+            }
+            descripts?.let {
+                textViewOnboardingpageFirstDescript1.text = it[0]
+                textViewOnboardingpageFirstDescript2.text = it[1]
+            }
+        }
+    }
+
+    private fun showSecondPage() {
+        onBoardingPageBinding.run {
+            lottieAnimationViewOnboardingpageSecond.apply {
+                lottieResIds?.let {
+                    setAnimation(it[0])
+                }
+                playAnimation()
+            }
+            descripts?.let {
+                textViewOnboardingpageSecondDescript1.text = it[0]
+                textViewOnboardingpageSecondDescript2.text = it[1]
+                textViewOnboardingpageSecondDescript3.text = it[2]
+            }
+        }
+    }
+
+    private fun showThirdPage() {
+        onBoardingPageBinding.run {
+            lottieAnimationViewOnboardingpageThirdCat.apply {
+                lottieResIds?.let {
+                    setAnimation(it[0])
+                }
+                playAnimation()
+            }
+            lottieAnimationViewOnboardingpageThirdDog.apply {
+                lottieResIds?.let {
+                    setAnimation(it[1])
+                }
+                playAnimation()
+            }
+            descripts?.let {
+                textViewOnboardingpageThirdDescript1.text = it[0]
+                textViewOnboardingpageThirdDescript2.text = it[1]
+                textViewOnboardingpageThirdDescript3.text = it[2]
+                textViewOnboardingpageThirdDescript4.text = it[3]
+            }
+        }
+    }
+
+    private fun showFourthPage() {
+        onBoardingPageBinding.run {
+            lottieAnimationViewOnboardingpageFourth.apply {
+                lottieResIds?.let {
+                    setAnimation(it[0])
+                }
+                playAnimation()
+            }
+            descripts?.let {
+                textViewOnboardingpageFourthDescript1.text = it[0]
+                textViewOnboardingpageFourthDescript2.text = it[1]
+            }
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(descripts: Array<String>, lottieResIds: IntArray, page: Int): Fragment {
+            return OnBoardingPageFragment().apply {
+                arguments = Bundle().apply {
+                    putStringArray(ARG_PARM1, descripts)
+                    putIntArray(ARG_PARM2, lottieResIds)
+                    putInt(ARG_PARM3, page)
+                }
+            }
+        }
+    }
+}
--- Ipet_customer/app/src/main/java/likelion/project/ipet_customer/ui/onboarding/OnboardFragment.kt
+++ Ipet_customer/app/src/main/java/likelion/project/ipet_customer/ui/onboarding/OnboardFragment.kt
@@ -1,22 +1,133 @@
 package likelion.project.ipet_customer.ui.onboarding
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import likelion.project.ipet_customer.R
 import likelion.project.ipet_customer.databinding.FragmentOnboardBinding
+import likelion.project.ipet_customer.ui.main.MainActivity
 
 class OnboardFragment : Fragment() {
 
     lateinit var fragmentOnboardBinding: FragmentOnboardBinding
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mainActivity = activity as MainActivity
         fragmentOnboardBinding = FragmentOnboardBinding.inflate(inflater)
+        initViewPager()
+        initEvent()
         return fragmentOnboardBinding.root
     }
+
+    private fun initEvent() {
+        setViewPagePosition()
+        setButtonTextToPageTransition()
+    }
+
+    private fun setButtonTextToPageTransition() {
+        fragmentOnboardBinding.run {
+            viewpagerOnboarding.registerOnPageChangeCallback(object :
+                ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    when (position) {
+                        OnboardingPage.FOURTH_PAGE.position -> buttonOnboardingNext.text =
+                            getString(R.string.start)
+
+                        else -> buttonOnboardingNext.text = getString(R.string.next)
+
+                    }
+                }
+            })
+        }
+    }
+
+    private fun setViewPagePosition() {
+        fragmentOnboardBinding.run {
+            buttonOnboardingNext.setOnClickListener {
+                viewpagerOnboarding.run {
+                    if (currentItem == END_PAGE) {
+
+                    } else {
+                        currentItem += 1
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initViewPager() {
+        fragmentOnboardBinding.run {
+            viewpagerOnboarding.adapter = ScreenSlidePagerAdapter(this@OnboardFragment)
+            viewpagerOnboarding.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            viewpagerOnboarding.isUserInputEnabled = false
+
+            TabLayoutMediator(
+                tabLayoutOnboardingDot,
+                viewpagerOnboarding
+            ) { tab, position -> }.attach()
+        }
+    }
+
+    private inner class ScreenSlidePagerAdapter(fragment: Fragment) :
+        FragmentStateAdapter(fragment) {
+        override fun getItemCount(): Int = PAGES_NUMBER
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                OnboardingPage.FIRST_PAGE.position -> {
+                    OnBoardingPageFragment.newInstance(
+                        descripts = arrayOf("아이펫", "반려동물 용품의 중심"),
+                        lottieResIds = intArrayOf(R.raw.raw_shopping_bag),
+                        page = OnboardingPage.FIRST_PAGE.position
+                    )
+                }
+
+                OnboardingPage.SECOND_PAGE.position -> {
+                    OnBoardingPageFragment.newInstance(
+                        descripts = arrayOf("함께 사면 더 저렴", "공동구매로 할인 혜택을", " 받아보세요."),
+                        lottieResIds = intArrayOf(R.raw.raw_sale),
+                        page = OnboardingPage.SECOND_PAGE.position
+                    )
+                }
+
+                OnboardingPage.THIRD_PAGE.position -> {
+                    OnBoardingPageFragment.newInstance(
+                        descripts = arrayOf("소중한", "우리 가족 아무거나", "먹일 수 없으니까", "전 제품 직접 검수"),
+                        lottieResIds = intArrayOf(R.raw.raw_cat, R.raw.raw_dog),
+                        page = OnboardingPage.THIRD_PAGE.position
+                    )
+                }
+
+                else -> {
+                    OnBoardingPageFragment.newInstance(
+                        descripts = arrayOf("평일 14시 이전 주문 시", "당일 배송"),
+                        lottieResIds = intArrayOf(R.raw.raw_deliver_box),
+                        page = OnboardingPage.FOURTH_PAGE.position
+                    )
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val PAGES_NUMBER = 4
+        private const val END_PAGE = 3
+    }
+}
+
+enum class OnboardingPage(val position: Int) {
+    FIRST_PAGE(0),
+    SECOND_PAGE(1),
+    THIRD_PAGE(2),
+    FOURTH_PAGE(3)
 }
