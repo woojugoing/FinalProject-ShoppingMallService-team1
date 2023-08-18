7a4dcfc5cd811ffbf7e3cc17a022e0b8df555052
Jbrochan <hyeongchanj@gmail.com>
2023-08-18 10:32:17 +0900

    Feat : 카카오 SDK 초기화 #13

--- Ipet_customer/app/src/main/java/likelion/project/ipet_customer/ui/login/LoginFragment.kt
+++ Ipet_customer/app/src/main/java/likelion/project/ipet_customer/ui/login/LoginFragment.kt
@@ -1,23 +1,30 @@
 package likelion.project.ipet_customer.ui.login
 
+import android.app.Application
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 import likelion.project.ipet_customer.R
 import likelion.project.ipet_customer.databinding.FragmentLoginBinding
+import likelion.project.ipet_customer.ui.main.MainActivity
 
 class LoginFragment : Fragment() {
 
     private lateinit var fragmentLoginBinding: FragmentLoginBinding
+    private lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         fragmentLoginBinding = FragmentLoginBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
         return fragmentLoginBinding.root
     }
 }
