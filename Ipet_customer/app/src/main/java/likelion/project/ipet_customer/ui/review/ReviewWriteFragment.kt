9e459abf1163e1da9adfe603f41df6375210dee3
Wise-99 <yerenkim@gmail.com>
2023-08-18 14:05:11 +0900

    Feat : 화면 UI 배치 #34

---
+++ Ipet_customer/app/src/main/java/likelion/project/ipet_customer/ui/review/ReviewWriteFragment.kt
@@ -0,0 +1,25 @@
+package likelion.project.ipet_customer.ui.review
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import likelion.project.ipet_customer.R
+import likelion.project.ipet_customer.databinding.FragmentReviewWriteBinding
+
+class ReviewWriteFragment : Fragment() {
+
+    lateinit var fragmentReviewWriteBinding: FragmentReviewWriteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentReviewWriteBinding = FragmentReviewWriteBinding.inflate(layoutInflater)
+
+
+
+        return fragmentReviewWriteBinding.root
+    }
+}
