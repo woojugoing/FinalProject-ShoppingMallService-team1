e868e8a4d0b7356d740c26bda64b659e7fd7f697
Wise-99 <yerenkim@gmail.com>
2023-08-21 16:14:09 +0900

    Feat : Toggle 버튼 선택 시 색상 변경 #45

--- Ipet_customer/app/src/main/java/likelion/project/ipet_customer/ui/home/HomeFragment.kt
+++ Ipet_customer/app/src/main/java/likelion/project/ipet_customer/ui/home/HomeFragment.kt
@@ -5,15 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import likelion.project.ipet_customer.R
+import likelion.project.ipet_customer.databinding.FragmentHomeBinding
+import likelion.project.ipet_customer.ui.main.MainActivity
 
 class HomeFragment : Fragment() {
+
+    lateinit var fragmentHomeBinding: FragmentHomeBinding
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        fragmentHomeBinding = FragmentHomeBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        fragmentHomeBinding.run {
+
+            // 시작 시 강아지 버튼 선택으로 지정하여 색상 변경
+            buttonHomeDog.setBackgroundColor(ContextCompat.getColor(mainActivity, R.color.rose_200))
+
+            toggleButtonHome.run {
+                addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
+                    if (isChecked){
+                        when(checkedId){
+                            // 강아지 버튼 클릭 시
+                            R.id.button_home_dog -> {
+                                buttonHomeDog.setBackgroundColor(ContextCompat.getColor(mainActivity, R.color.rose_200))
+                                buttonHomeCat.setBackgroundColor(ContextCompat.getColor(mainActivity, R.color.white))
+                            }
+
+                            // 고양이 버튼 클릭 시
+                            R.id.button_home_cat -> {
+                                buttonHomeCat.setBackgroundColor(ContextCompat.getColor(mainActivity,R.color.rose_200))
+                                buttonHomeDog.setBackgroundColor(ContextCompat.getColor(mainActivity, R.color.white))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return fragmentHomeBinding.root
     }
 }
