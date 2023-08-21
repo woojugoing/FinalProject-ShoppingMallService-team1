7a5f225ea6bf6869f998dbb5bbcc8b337c255f12
dltkd1395 <dltkd13956@naver.com>
2023-08-21 17:44:57 +0900

    Feat : firestore에 Seller 데이터를 가져오고 로그인 성공 시 홈 화면으로 이동, 회원가입 화면 이동 #48

---
+++ Ipet_seller/app/src/main/java/likelion/project/ipet_seller/repository/SellerRepository.kt
@@ -0,0 +1,10 @@
+package likelion.project.ipet_seller.repository
+
+import likelion.project.ipet_seller.db.remote.SellerDataSource
+import likelion.project.ipet_seller.model.Seller
+
+class SellerRepository {
+    private val sellerDataSource = SellerDataSource()
+
+    suspend fun getSellerInfo(seller: Seller) = sellerDataSource.getSellerInfo(seller)
+}
