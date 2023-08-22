ebcabf4ead9ec3bb9075b3777252a44e46152936
Jbrochan <hyeongchanj@gmail.com>
2023-08-22 11:35:25 +0900

    Feat : 카카오 로그인 ID 토큰 사용자 인증 정보 추출 #13

--- Ipet_customer/app/src/main/java/likelion/project/ipet_customer/ui/login/LoginFragment.kt
+++ Ipet_customer/app/src/main/java/likelion/project/ipet_customer/ui/login/LoginFragment.kt
@@ -2,6 +2,7 @@ package likelion.project.ipet_customer.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -41,13 +42,11 @@ class LoginFragment : Fragment() {
                     parsingLoginInfo(KAKAO_LOGIN)
                 }
             }
-
             buttonLoginNaver.run{
                 setOnClickListener {
                     parsingLoginInfo(NAVER_LOGIN)
                 }
             }
-
             buttonLoginGoogle.run{
                 setOnClickListener {
                     parsingLoginInfo(GOOGLE_LOGIN)
@@ -65,7 +64,12 @@ class LoginFragment : Fragment() {
                         Log.e("login", "카카오 로그인 실패", error)
                     }
                     else if (token != null) {
-                        Log.i("login", "카카오 로그인 성공 ${token.idToken}")
+                        val idTokenList = token.idToken?.split(".")
+                        if(idTokenList != null){
+                            // 사용자 규격 정보
+                            val idTokenPayload = Base64.decode(idTokenList[1], Base64.DEFAULT).toString(Charsets.UTF_8)
+                            Log.i("login", "카카오 로그인 성공 $idTokenPayload")
+                        }
                     }
                 }
             }
