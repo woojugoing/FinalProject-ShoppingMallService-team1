3a6053823fbdb12ef9ba610a3f1fe254925c3fea
Wise-99 <yerenkim@gmail.com>
2023-08-23 13:47:02 +0900

    Feat : UI 배치 및 recyclerView item, toolBar menu xml 생성 #80

---
+++ Ipet_seller/app/src/main/java/likelion/project/ipet_seller/ui/product/ProductListFragment.kt
@@ -0,0 +1,19 @@
+package likelion.project.ipet_seller.ui.product
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import likelion.project.ipet_seller.R
+
+class ProductListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_list, container, false)
+    }
+}
