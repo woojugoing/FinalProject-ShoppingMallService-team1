e07e6f029a79a2a661066df6921baa7c0213cca5
Jbrochan <hyeongchanj@gmail.com>
2023-08-23 14:16:05 +0900

    Feat : 구매자 데이터 소스 클래스 구현 #13
    - Firestore 데이터 소스 클래스 구현
    - Model에 해당하는 Customer 클래스 구현

--- Ipet_customer/app/src/main/java/likelion/project/ipet_customer/db/remote/CustomerDataSource.kt
+++ Ipet_customer/app/src/main/java/likelion/project/ipet_customer/db/remote/CustomerDataSource.kt
@@ -1,8 +1,47 @@
 package likelion.project.ipet_customer.db.remote
 
+import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import likelion.project.ipet_customer.model.Customer
 
 class CustomerDataSource {
     private val db = Firebase.firestore
+
+    // 초기 유저의 정보를 Firestore에 저장
+    fun signUp(customerId: String, customerName: String){
+        val customer = Customer(customerId, customerName)
+        db.collection("Customer").add(customer).addOnSuccessListener {
+            Log.i("login", "$customerId 신규 로그인 성공")
+        }.addOnFailureListener {
+            Log.i("login", "$customerId 신규 로그인 실패")
+        }
+    }
+
+    // 기존 유저의 정보를 Firestore에서 추출
+    fun signIn(customerId: String){
+        db.collection("Customer")
+            .get()
+            .addOnSuccessListener {
+                Log.i("login", "기존 로그인 성공")
+            }
+            .addOnFailureListener {
+                Log.i("login", "기존 로그인 실패")
+            }
+    }
+
+    // 사용자의 정보 중복 검사
+    fun searchCustomer(customerId: String) : Boolean{
+        var result = false
+        db.collection("Customer")
+            .whereEqualTo("customerId", customerId)
+            .get()
+            .addOnSuccessListener {
+                result = true
+            }
+            .addOnFailureListener {
+                result = false
+            }
+        return result
+    }
 }
