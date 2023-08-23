270b8f211d3c56839666d7e3631c89352ceda7c2
dltkd1395 <dltkd13956@naver.com>
2023-08-23 15:39:41 +0900

    Feat : 판매자 정보 업데이트 #74

--- Ipet_seller/app/src/main/java/likelion/project/ipet_seller/ui/home/HomeFragment.kt
+++ Ipet_seller/app/src/main/java/likelion/project/ipet_seller/ui/home/HomeFragment.kt
@@ -11,27 +11,38 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import likelion.project.ipet_seller.R
 import likelion.project.ipet_seller.databinding.FragmentHomeBinding
+import likelion.project.ipet_seller.ui.main.MainActivity
 
 class HomeFragment : Fragment() {
 
     lateinit var fragmentHomeBinding: FragmentHomeBinding
     lateinit var viewModel: HomeViewModel
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mainActivity = activity as MainActivity
         fragmentHomeBinding = FragmentHomeBinding.inflate(inflater)
-        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
-        viewModel.fetchOrders("test")
+        viewModel = ViewModelProvider(this, HomeViewModelFactory(mainActivity))[HomeViewModel::class.java]
+        initViewModel()
         observe()
         return fragmentHomeBinding.root
+    }
+
+    private fun initViewModel() {
+        viewModel.run {
+            fetchOrders()
+            fetchSeller()
+        }
     }
 
     private fun observe() {
         lifecycleScope.launch {
             viewModel.uistate.collect {
                 fragmentHomeBinding.run {
+                    textViewHomeSellerName.text = "판매자 ${it.seller?.sellerName} 님"
                     textViewHomeNewOrderCount.text = "신규 주문: ${it.orderCount}건"
                     textViewHomePreparingCount.text = it.preparingCount.toString()
                     textViewHomeDeliveringCount.text = it.deliveringCount.toString()
--- Ipet_seller/app/src/main/java/likelion/project/ipet_seller/ui/home/HomeViewModel.kt
+++ Ipet_seller/app/src/main/java/likelion/project/ipet_seller/ui/home/HomeViewModel.kt
@@ -1,6 +1,6 @@
 package likelion.project.ipet_seller.ui.home
 
-import android.util.Log
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -9,17 +9,37 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import likelion.project.ipet_seller.model.Order
+import likelion.project.ipet_seller.model.Seller
 import likelion.project.ipet_seller.repository.OrderRepository
+import likelion.project.ipet_seller.repository.SellerRepository
 
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(context: Context) : ViewModel() {
     private val orderRepository = OrderRepository()
+    private val sellerRepository = SellerRepository(context)
 
     private val _uiState = MutableStateFlow(UiState())
     val uistate = _uiState.asStateFlow()
 
-    fun fetchOrders(sellerId: String) {
+    fun fetchSeller() {
         viewModelScope.launch {
+            val id = sellerRepository.readSellerIdToLocal().first
+            val pw = sellerRepository.readSellerIdToLocal().second
+            sellerRepository.getSellerInfo(Seller(sellerId = id, sellerPw = pw)).collect {
+                it.onSuccess { seller ->
+                    _uiState.update {
+                        it.copy(
+                            seller = seller
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    fun fetchOrders() {
+        viewModelScope.launch {
+            val sellerId = sellerRepository.readSellerIdToLocal().first
             orderRepository.getOrders(sellerId).collect {
                 it.onSuccess { orders ->
                     _uiState.update {
@@ -38,6 +58,7 @@ class HomeViewModel : ViewModel() {
 }
 
 data class UiState(
+    val seller: Seller? = null,
     val orderList: List<Order>? = null,
     val orderCount: Int = 0,
     val preparingCount: Int = 0,
