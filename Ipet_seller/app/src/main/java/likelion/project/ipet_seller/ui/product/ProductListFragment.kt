1eb604538015c5d48e3f5929d3fa2d8599ad89ac
Wise-99 <yerenkim@gmail.com>
2023-08-23 14:10:55 +0900

    Feat : 코드를 통한 menuItem 색상 변경 #80

--- Ipet_seller/app/src/main/java/likelion/project/ipet_seller/ui/product/ProductListFragment.kt
+++ Ipet_seller/app/src/main/java/likelion/project/ipet_seller/ui/product/ProductListFragment.kt
@@ -1,19 +1,49 @@
 package likelion.project.ipet_seller.ui.product
 
+import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.core.view.children
+import androidx.core.view.forEach
+import androidx.core.widget.ImageViewCompat
 import likelion.project.ipet_seller.R
+import likelion.project.ipet_seller.databinding.FragmentProductListBinding
+import likelion.project.ipet_seller.ui.main.MainActivity
 
 class ProductListFragment : Fragment() {
+
+    lateinit var fragmentProductListBinding: FragmentProductListBinding
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_list, container, false)
+        fragmentProductListBinding = FragmentProductListBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        fragmentProductListBinding.run {
+            toolbarProductList.run {
+                inflateMenu(R.menu.menu_productlist)
+
+                val color = ContextCompat.getColorStateList(mainActivity, R.color.brown_200)
+                val menuItem = menu.findItem(R.id.item_add_product)
+
+                val icon = menuItem.icon
+                icon?.let {
+                    val wrappedDrawable = DrawableCompat.wrap(it)
+                    DrawableCompat.setTintList(wrappedDrawable, color)
+                    menuItem.icon = wrappedDrawable
+                }
+            }
+        }
+
+        return fragmentProductListBinding.root
     }
 }
