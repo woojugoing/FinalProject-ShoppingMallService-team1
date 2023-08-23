a8e95e919d915ab2642b9c936f74170c33c65a54
Jbrochan <hyeongchanj@gmail.com>
2023-08-23 09:50:18 +0900

    Feat : Firestore 인스턴스 초기화 #70

---
+++ Ipet_customer/app/src/main/java/likelion/project/ipet_customer/db/remote/CustomerDataSource.kt
@@ -0,0 +1,8 @@
+package likelion.project.ipet_customer.db.remote
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class CustomerDataSource {
+    private val db = Firebase.firestore
+}
