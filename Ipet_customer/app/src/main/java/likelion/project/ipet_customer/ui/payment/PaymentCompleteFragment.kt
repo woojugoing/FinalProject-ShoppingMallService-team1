b36a4f1681ea9b730d3a45ec80ca965d5b6a31f0
juseong <wjdwntjd55@naver.com>
2023-08-24 16:51:56 +0900

    Feat : 버튼 클릭시 화면 이동 구현 #52 - 결제 완료 화면 - 홈 화면으로 이동 하는 버튼 구현 - 내 정보 화면으로 이동하는 버튼 구현 - 결제 완료 화면에서 뒤로가기 막는 기능 구현

--- Ipet_customer/app/src/main/java/likelion/project/ipet_customer/ui/payment/PaymentCompleteFragment.kt
+++ Ipet_customer/app/src/main/java/likelion/project/ipet_customer/ui/payment/PaymentCompleteFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import likelion.project.ipet_customer.databinding.FragmentPaymentCompleteBinding
 import likelion.project.ipet_customer.ui.main.MainActivity
 
@@ -12,7 +13,20 @@ class PaymentCompleteFragment : Fragment() {
 
     lateinit var fragmentPaymentCompleteBinding: FragmentPaymentCompleteBinding
     lateinit var mainActivity: MainActivity
+    lateinit var callback: OnBackPressedCallback
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 뒤로가기 버튼을 막기 위한 callback을 생성하고 등록한다
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // 여기서 아무것도 하지 않으면 뒤로가기 동작이 막힌다.
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,9 +37,36 @@ class PaymentCompleteFragment : Fragment() {
 
         fragmentPaymentCompleteBinding.run {
 
+            // 홈 화면으로 이동
+            buttonPaymentCompleteGoMain.run {
+                setOnClickListener {
+
+                    val fragmentManager = mainActivity.supportFragmentManager
+                    // 모든 백스택 항목 제거
+                    for (i in 0 until fragmentManager.backStackEntryCount) {
+                        fragmentManager.popBackStackImmediate()
+                    }
+                    mainActivity.replaceFragment(MainActivity.HOME_FRAGMENT, false, null)
+
+                }
+            }
+
+            // 주문확인 하기 (내정보 화면으로 이동)
+            buttonPaymentCompleteGoOrder.run {
+                setOnClickListener {
+
+                    val fragmentManager = mainActivity.supportFragmentManager
+                    // 모든 백스택 항목 제거
+                    for (i in 0 until fragmentManager.backStackEntryCount) {
+                        fragmentManager.popBackStackImmediate()
+                    }
+
+                    mainActivity.replaceFragment(MainActivity.USER_INFO_MAIN_FRAGMENT, false, null)
+                }
+            }
+
         }
 
         return fragmentPaymentCompleteBinding.root
     }
-
 }
